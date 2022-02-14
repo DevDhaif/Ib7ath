22a40826d4c1f7c6fe09ddf6ffe776f8404799a8
DevDhaif <devdhaif@gmail.com>
2022-02-14 16:48:28 +0200

    Fix Router v5 issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import { Footer } from './components/Footer';
 import {Navbar} from './components/Navbar'
-import {Routes} from './components/Routes'
+import {Routes, Routess} from './components/Routes'
 
 function App() {
   const [darkTheme,setDarkTheme]=useState(true)
+  
   return (
     <div className={darkTheme?'dark':''}>
       <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-50 min-h-screen'>
-          <Navbar/>
+          <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
           <Footer/>
-          <Routes/>
+          <Routess/>
       </div>
     </div>
   );
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
-export const Routes = () => {
+import {Routes,  BrowserRouter as Router,Route,Switch, Navigate,Redirect} from 'react-router-dom'
+import App from '../App'
+import { Navbar } from './Navbar'
+import { Results } from './Results'
+export const Routess = () => {
   return (
-    <div>Routes</div>
+    
+      <div className='p-4'>
+          <Routes>
+            <Route exat path='/' element={<Navigate replace to="/search" />}/>
+            {/* 
+            <Route exat path={['/search']} element={<Results/>}/>
+            */}
+            {["/search", "/images",'news','videos'].map(path => (
+              <Route exat
+                key={'Results'}
+                path={path}
+                element={<Results />}
+              />
+            ))}
+          </Routes>     
+      </div>
+    
   )
 }
